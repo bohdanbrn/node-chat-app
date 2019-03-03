2412085eb7e14c2b87aba82b48fd1ae643c3c92e
bohdanbrn <bohdanbrn@gmail.com>
2019-03-03 23:07:20 +0200

    Socket.io Events

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,10 +1,25 @@
 const socket = io()
 
-socket.on('countUpdated', (count) => { 
-    console.log('The count has been updated!', count);
-});
+// socket.on('countUpdated', (count) => { 
+//     console.log('The count has been updated!', count);
+// });
 
-document.querySelector('#increment').addEventListener('click', () => {
-    console.log('clicked')
-    socket.emit('increment');
+// document.querySelector('#increment').addEventListener('click', () => {
+//     console.log('clicked')
+//     socket.emit('increment');
+// })
+
+socket.on('message', (message) => {
+    console.log(message);
+})
+
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let message = e.target.elements.message.value
+
+    console.log(message);
+
+    // socket.emit('sendMessage', message)
+    // console.log(this);
 })
