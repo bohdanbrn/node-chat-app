012aad771f711f067d642e9528333d8abdcc1a02
bohdanbrn <bohdanbrn@gmail.com>
2019-03-05 23:53:22 +0200

    s17 l159 (Event Acknowledgements)

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,29 +1,38 @@
 const socket = io()
 
 socket.on('message', (message) => {
-    console.log(message);
+    console.log(message)
 })
 
 document.querySelector('#message-form').addEventListener('submit', (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
     let message = e.target.elements.message.value
 
-    console.log(message);
+    socket.emit('sendMessage', message, (error) => {
+        if (error) {
+            return console.log(error)
+        }
 
-    // socket.emit('sendMessage', message)
-    // console.log(this);
+        console.log("Message delivered!")
+    })
 })
 
 document.querySelector('#send-location').addEventListener('click', () => {
     if (!navigator.geolocation) {
-        return alert("Geolocation is not supported by your browser.");
+        return alert("Geolocation is not supported by your browser.")
     }
 
     navigator.geolocation.getCurrentPosition((position) => {
         socket.emit('sendLocation', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
+        }, (error) => {
+            if (error) {
+                return console.log(error)
+            }
+
+            console.log("Location shared!")
         })
     })
-});
+})
