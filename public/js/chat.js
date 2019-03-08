610f9b8e0cabe5a78440d3e10eb5e35b4949614c
bohdanbrn <bohdanbrn@gmail.com>
2019-03-08 18:56:28 +0200

    s17 l167 (Socket.io Rooms)

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,6 +10,9 @@ const $messages = document.querySelector('#messages')
 const messageTemplate = document.querySelector('#message-template').innerHTML
 const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
 
+// Options
+const {username, room} = Qs.parse(location.search, {ignoreQueryPrefix: true})
+
 socket.on('message', (message) => {
     const html = Mustache.render(messageTemplate, {
         message: message.text,
@@ -23,7 +26,7 @@ socket.on('LocationMessage', (message) => {
         url: message.url,
         createdAt: moment(message.createdAt).format('DD.MMMM HH:mm')
     })
-    $messages.insertAdjacentHTML('beforeend', html);
+    $messages.insertAdjacentHTML('beforeend', html)
 })
 
 
@@ -74,3 +77,5 @@ $sendLocationButton.addEventListener('click', () => {
         })
     })
 })
+
+socket.emit('join', {username, room})
